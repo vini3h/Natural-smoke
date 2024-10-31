259d5f3a7b838607a49946d71022883eb07ae118
Vinicius Silva <134015186+vini3h@users.noreply.github.com>
2024-10-31 10:50:25 -0300

    Adicionado novas páginas e favicon

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,16 @@
 document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const items = document.querySelectorAll('.side-item a');
+    const currentPage = window.location.pathname.split('/').pop(); // Obtém o nome da página atual
+
+    items.forEach(item => {
+        const link = item.getAttribute('href');
+
+        if (link === currentPage) {
+            item.parentElement.classList.add('active'); // Adiciona a classe active ao item pai
+        }
+    });
+});
