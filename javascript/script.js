e9798a98dd9f946a4f3a5d00ccfcab155ab2af65
Vinicius Silva <134015186+vini3h@users.noreply.github.com>
2024-11-05 10:56:38 -0300

    Adicionado slide de imagens

--- javascript/script.js
+++ javascript/script.js
@@ -14,3 +14,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+let contador = 1;
+
+setInterval( function(){
+    document.getElementById('slide' + contador).checked = true;
+    contador ++;
+
+    if(contador > 4) {
+        contador =1;
+    }
+
+}, 5000)
